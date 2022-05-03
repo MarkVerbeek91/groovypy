71b1b5d45327f229c20dc4f6e8163c265a2112b4
Mark Verbeek <mark.verbeek@asml.com>
2022-05-03 15:31:55 +0200

    add expand vars to shell class

--- src/groovypy/elements/shell.py
+++ src/groovypy/elements/shell.py
@@ -1,4 +1,6 @@
+from os.path import expandvars
 import subprocess
+import shlex
 
 
 class Shell:
@@ -7,7 +9,8 @@ class Shell:
         self.command = command
 
     def __call__(self):
+        cmd = " ".join([expandvars(x) for x in shlex.split(self.command)])
         try:
-            subprocess.run(self.command, shell=True, check=True)
+            subprocess.run(cmd, shell=True, check=True)
         except subprocess.CalledProcessError as err:
             print(f"Command did not run: {err}")
